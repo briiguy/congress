dc493568d05916eaffc1f15d5187b8ba07eac495
Brian Evasiuk <brianevasiuk@gmail.com>
2016-06-17 15:56:16 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,48 +1,119 @@
 console.log('hello world')
 
-// https://congress.api.sunlightfoundation.com/legislators?apikey=[your key]
+// METADATA URL EXAMPLE
+// http://openstates.org/api/v1/metadata/tx/?apikey=0e85724a8f924c6aba8bd576df364eb7
 
-//the dollar variable holds the entire jquery library
-console.log($)
+// LEGISLATOR URL EXAMPLE
+// http://openstates.org/api/v1/legislators/?state=tx&apikey=0e85724a8f924c6aba8bd576df364eb7
 
-//$.getJSON makes a request to the input url. it returns a promise, the very special object that is described 
-// at length below
-var congressionalPromise = $.getJSON('https://congress.api.sunlightfoundation.com/legislators?apikey=721e2620a76a4a4d93562fa154f88811')
+// define some global variables
+var legislatorsUrlRoot = "http://openstates.org/api/v1/legislators/",
+	stateUrlRoot = "http://openstates.org/api/v1/metadata/",
+	apiKey = "0e85724a8f924c6aba8bd576df364eb7",
+	legislatorParams = {
+		apikey: apiKey,
+	},
+	stateParams = {
+		apikey: apiKey
+	},
+	containerNode = document.querySelector('#container')
+var stateQuery = function(stateInput){
+	stateUrlRoot += stateInput + '/'
+	legislatorParams.state = stateInput
+}
+stateQuery(inputValue)
 
-
-var statesContainer = document.querySelector("#statesContainer")
-
-
-// we define handleData with the api's response as an input. But this api response is not mentioned anywhere else
-// in this code! Confusing, eh? That's because this is handled in the jquery source code. See the comment below 
-// on the .then() element.
-var handleData = function(apiResponse) {
-    console.log(apiResponse)
-    var legArray = apiResponse.results
-    //initialize a string of html
-    var htmlString = '<h1>We have ' + apiResponse.count + ' congressional cuties to show you today!</h1>'
-
-    for (var i = 0; i < legArray.length; i ++) {
-        var legislatorObject = legArray[i]
-        console.log(legislatorObject.state_name)
-        
-        //concatenate (add) the state and name of each legislator as we see them.
-        htmlString += '<li class="stateName">state:' + legislatorObject.state_name + '</li>' 
-        htmlString += '<li class="firstName">name:' + legislatorObject.first_name + '</li>'
-        htmlString += '<li class="email">email:' + legislatorObject.oc_email + '</li>' 
-        htmlString += '<li class="twitter">twitter:' + legislatorObject.twitter_id+ '</li>'  
+var genParamString = function(paramObject) {
+    var outputString = '?'
+    for (var key in paramObject) {
+     	outputString += key + '=' + paramObject[key] + '&'
     }
-    // we've now accumulated scores of <p> tags into our htmlString. if we set it as the innerHTML
-    // of the statesContainer, the browser will rebuild its DOM tree according to the html structure 
-    // that we added to the statesContainer
-    statesContainer.innerHTML = htmlString
+    return outputString.substr(0,outputString.length - 1)
 }
 
+// build the urls we need
+var legislatorsUrlFull = legislatorsUrlRoot + genParamString(legislatorParams)
+var stateUrlFull = stateUrlRoot + genParamString(stateParams)
 
-// What we're doing here is passing a function along to our promise. The promise says "OK i got your function.
-// When the request is fulfilled, and the data is ready, I will pass the data into your function". That passing-in
-// is in the jquery source. That's why we define handleData with an input that we ourselves never pass in.
-congressionalPromise.then(handleData)
+// request data from each url, store the promises that are returned
+var legislatorPromise = $.getJSON(legislatorsUrlFull)
+var statePromise = $.getJSON(stateUrlFull)
 
-// summary: promise.then() takes as input a function. It queues up that function to run when the promise is fulfilled. 
-// When the promise is fulfilled (i.e. the data is ready), the promise (and not us) will invoke that function.
+
+// define functions that will handle the data when it's ready. note that
+// each of these functions takes a response object as input. that's because
+// when the promise object invokes them, it will pass as input the response 
+// to our request.
+var stateDataHandler = function(responseObject) {
+	console.log('eyyyy we got some state data!!!')
+	console.log(responseObject)
+
+	// build an html string
+	var htmlString = ''
+	var stateName = responseObject.name,
+		legislatureName = responseObject.legislature_name,
+		legislatureUrl = responseObject.legislature_url
+
+	htmlString += '<p class="stateName">state: ' + stateName + '</p>'
+	htmlString += '<p class="legName">name: ' + legislatureName + '</p>'
+	htmlString += '<a href="' + legislatureUrl + '">website</a>'
+
+	// write it into the container
+	var leftContainer = document.querySelector("#leftCol")
+	leftContainer.innerHTML = htmlString
+}
+
+var legislatorDataHandler = function(legislatorsArray) {
+	console.log('yooo we got some legislator data, i guess')
+	// "full_name": "Dan Patrick",
+	// "+district_address": " 11451 Katy Fwy, Suite 209\nHouston, TX 77079\n(713) 464-0282",
+	// "photo_url": "http://www.legdir.legis.state.tx.us/FlashCardDocs/images/Senate/small/A1430.jpg",
+
+	var htmlCards = ''
+	for (var i = 0; i < 10; i ++) {
+		var legObject = legislatorsArray[i],
+			name = legObject.full_name,
+			addy = legObject['+district_address'],
+			imgSrc = legObject.photo_url
+		if (addy === undefined) {
+			addy = "not listed"
+		}
+		htmlCards += '<div class="legCard">'
+		htmlCards += '<div class="portrait">'
+		htmlCards += 	'<img src="' + imgSrc + '">'
+		htmlCards += '</div>'
+		htmlCards += '<div class="legData">'
+		htmlCards += 	'<h2 class="name">name: ' + name + '</h2>'
+		htmlCards += 	'<p class="address">address: ' + addy + '</p>'
+		htmlCards += '</div>'
+		htmlCards += '</div>'
+	}
+	var rightContainer = document.querySelector('#rightCol')
+	rightContainer.innerHTML += htmlCards
+}
+
+// hand our functions over to the promise objects, so they 
+// can be invoked when the data is ready.
+
+var searchBar = document.querySelector('input')
+var searchFunction = function(eventObj){
+	// If enter key is pressed
+	if(eventObj.keyCode === 13){
+		var stateQuery = function(stateInput){
+	stateUrlRoot += stateInput + '/'
+	legislatorParams.state = stateInput
+}
+		// Extract value that user typed
+		var inputElement = eventObj.target
+		var inputValue = inputElement.value
+		// Make a custom query with the input value
+		statePromise.then(stateDataHandler)
+legislatorPromise.then(legislatorDataHandler)
+		stateQuery(inputValue)
+	}
+}
+
+searchBar.addEventListener('keydown',searchFunction)
+
+
+
